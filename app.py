a1e75155864f1297424f8fb88893ffc96afc2c42
xingzhihe <xingzhihe@itbox.cn>
2025-05-06 17:18:37 +0800

    修改端口

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return "this is from code-server website"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=6000)
+    app.run(debug=True, host="0.0.0.0", port=6666)
