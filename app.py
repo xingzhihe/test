830defd7a7ed0aa2be905587384bf99885c9a12a
xingzhihe <xingzhihe@itbox.cn>
2025-05-06 17:31:42 +0800

    联调验证

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "this is from code-server website"
+    return "this is from code-server website for python flask"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=6666)
